6df44392c2f66404e9ee66be8a8ddfd47e4b9987
Justin Cranford <justincranford@hotmail.com>
2017-12-08 14:28:13 -0500

    Security enhancements.

--- jcutil-xml/src/main/java/com/github/justincranford/jcutils/XmlUtil.java
+++ jcutil-xml/src/main/java/com/github/justincranford/jcutils/XmlUtil.java
@@ -3,10 +3,16 @@ package com.github.justincranford.jcutils;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+/**
+ * Cache DocumentBuilderFactory (static) and DocumentBuilder (ThreadLocal)
+ * for faster XML parser performance when calling DocumentBuilder.parse(InputStream).
+ */
+@SuppressWarnings("synthetic-access")
 public class XmlUtil {
 	private static final Logger LOG = Logger.getLogger(XmlUtil.class.getName());
 
@@ -14,11 +20,60 @@ public class XmlUtil {
 		// prevent instantiation of this class
 	}
 
-	// Cache DocumentBuilderFactory (static) and DocumentBuilder (ThreadLocal) for faster XML parsing performance when calling DocumentBuilder.parse(InputStream).
 	/*package*/ static final DocumentBuilderFactory DOCUMENT_BUILDER_FACTORY = DocumentBuilderFactory.newInstance();	// slow
 	static {
-		DOCUMENT_BUILDER_FACTORY.setNamespaceAware(true);
+		try {
+			/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			// https://www.owasp.org/index.php/XML_External_Entity_(XXE)_Prevention_Cheat_Sheet#JAXP_DocumentBuilderFactory.2C_SAXParserFactory_and_DOM4J
+			/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+			// This is the PRIMARY defense. If DTDs (doctypes) are disallowed, almost all XML entity attacks are prevented
+			// Xerces 2 only - http://xerces.apache.org/xerces2-j/features.html#disallow-doctype-decl
+			DOCUMENT_BUILDER_FACTORY.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+
+			// If you can't completely disable DTDs, then at least do the following:
+			// Xerces 1 - http://xerces.apache.org/xerces-j/features.html#external-general-entities
+			// Xerces 2 - http://xerces.apache.org/xerces2-j/features.html#external-general-entities
+			DOCUMENT_BUILDER_FACTORY.setFeature("http://xml.org/sax/features/external-general-entities", true);
+
+			// Xerces 1 - http://xerces.apache.org/xerces-j/features.html#external-parameter-entities
+			// Xerces 2 - http://xerces.apache.org/xerces2-j/features.html#external-parameter-entities
+			// JDK7+ - http://xml.org/sax/features/external-parameter-entities    
+			DOCUMENT_BUILDER_FACTORY.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+
+			// Disable external DTDs as well
+			DOCUMENT_BUILDER_FACTORY.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+
+			// and these as well, per Timothy Morgan's 2014 paper: "XML Schema, DTD, and Entity Attacks"
+			DOCUMENT_BUILDER_FACTORY.setXIncludeAware(false);
+			DOCUMENT_BUILDER_FACTORY.setExpandEntityReferences(false);
+
+			// And, per Timothy Morgan: "If for some reason support for inline DOCTYPEs are a requirement, then 
+			// ensure the entity settings are disabled (as shown above) and beware that SSRF attacks
+			// (http://cwe.mitre.org/data/definitions/918.html) and denial 
+			// of service attacks (such as billion laughs or decompression bombs via "jar:") are a risk."
+
+			/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			// EDC Security Assurance - https://sottconfluence.corporate.datacard.com/display/secassur/Avoiding+XML-Based+Attacks
+			/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+			// From sample code. This enforces certain limits on resource usage.
+			DOCUMENT_BUILDER_FACTORY.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);	// "http://javax.xml.XMLConstants/feature/secure-processing"
+
+			// From sample code. No explanation given.
+			DOCUMENT_BUILDER_FACTORY.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
+
+			//////////////////////
+			// Additional settings
+			//////////////////////
+
+			DOCUMENT_BUILDER_FACTORY.setFeature("http://xml.org/sax/features/namespaces", false);
+			DOCUMENT_BUILDER_FACTORY.setFeature("http://xml.org/sax/features/validation", false);
+		} catch (ParserConfigurationException pce) {
+			throw new RuntimeException("Unexpected exception. Cannot initialize securely.", pce);
+		}
 	}
+
 	/*package*/ static final ThreadLocal<DocumentBuilder> DOC_DOM_BUILDER = new ThreadLocal<DocumentBuilder>() {	// NOSONAR Remove this unused "DOC_DOM_BUILDER" private field.
 		@Override
 		public DocumentBuilder initialValue() {
